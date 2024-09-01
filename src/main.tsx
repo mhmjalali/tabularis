498f96447bb6dde740813fcab22f923f153d5d37
Mohammad Jalali <muhammad.art79@gmail.com>
2024-09-01 12:27:26 +0330

    husky

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,5 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App/>
-        <>hello babe</>
-        <>asdasda dsd</>
     </StrictMode>,
 );
